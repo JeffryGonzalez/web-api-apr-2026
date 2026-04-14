fd42fe70621b9496b40213129f6248dd31dd3c7b
Jeff Gonzalez <jeff@hypertheory.com>
2026-04-14 16:45:32 -0400

    demo

---
+++ src/SoftwareCenter/SoftwareCenterSolution/Software.Api/PdfController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Software.Api;
+
+public class PdfController : ControllerBase
+{
+    [HttpPost("/pdf")]
+    public async Task<ActionResult> GeneratePdf([FromBody] string html, [FromServices] RemoteServiceClient remoteServiceClient)
+    {
+
+        // do your validation...
+        var response = await remoteServiceClient.SendRequestAsync(new RemoteServiceClient.RemoteServiceRequest
+        {
+            FormType = "pdf",
+            Body = html
+        }, CancellationToken.None);
+
+        return Ok(response);    
+    }
+}
---
+++ src/SoftwareCenter/SoftwareCenterSolution/Software.Api/RemoteServiceClient.cs
@@ -0,0 +1,26 @@
+﻿namespace Software.Api;
+
+public class RemoteServiceClient(HttpClient httpClient)
+{
+
+    public async Task<RemoteServiceResponse> SendRequestAsync(RemoteServiceRequest request, CancellationToken token)
+    {
+
+        var httpResponse = await httpClient.PostAsJsonAsync<RemoteServiceRequest>("/pathtopostto", request, token);
+        
+        httpResponse.EnsureSuccessStatusCode(); 
+
+        return await httpResponse.Content.ReadFromJsonAsync<RemoteServiceResponse>(cancellationToken: token) ?? new RemoteServiceResponse();
+
+    }
+
+public record RemoteServiceRequest
+{
+    public string FormType { get; set; }
+    public string Body { get; set; }
+}
+
+public record RemoteServiceResponse
+{
+    public string Result { get; set; }
+}
